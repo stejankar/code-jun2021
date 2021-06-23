9fe6a211a51c6af1e22ad2ca3c40458ffdf503f8
stejankar <replytosanju@hotmail.com>
2021-06-23 09:02:44 -0400

    Add files via upload

--- src/main/java/FindWordInFile.java
+++ src/main/java/FindWordInFile.java
@@ -1,26 +1,25 @@
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FilterFunction;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.SparkSession;
 
 public class FindWordInFile {
     public static void main(String[] args) {
-        SparkConf sparkConf = new SparkConf().setAppName("Read Multiple Text Files to RDD")
-                .set("spark.executor.memory","2g");
-        // start a spark context
-        JavaSparkContext sc = new JavaSparkContext(sparkConf);
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("WordInFile")
+                .getOrCreate();
+
         String inputFile = "hdfs://sample.txt";
-        String result = wordInFile(inputFile, sc, "world");
+        String result = wordInFile(inputFile, spark, "world");
         System.out.println(result);
-        sc.stop();
+        spark.stop();
     }
 
-    public static String wordInFile(String inputFile, JavaSparkContext sc, String word) {
-        JavaRDD<String> lines = sc.textFile(inputFile, 1);
-        boolean wordExists = false;
-        for(String line:lines.collect()){
-            if(line.contains(word)) {
-                return String.format("Keyword \"%s\" exists in given file", word);
-            }
+    public static String wordInFile(String inputFile, SparkSession spark, String word) {
+        Dataset<String> lines = spark.read().textFile(inputFile).cache();
+        long linesWithWord = lines.filter((FilterFunction<String>) line -> line.contains(word)).count();
+        if(linesWithWord > 0L) {
+            return String.format("Keyword \"%s\" exists in given file", word);
         }
         return String.format("Keyword \"%s\" does not exist in given file", word);
     }
